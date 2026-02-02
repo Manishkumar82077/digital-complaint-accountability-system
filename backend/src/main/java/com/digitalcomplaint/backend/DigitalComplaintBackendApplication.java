aa9226f0b104384c1910b04edbafcb99220a4569
manish <manishkumar7543811190@gmail.com>
2026-02-03 05:21:33 +0530

    set up backend

---
+++ backend/src/main/java/com/digitalcomplaint/backend/DigitalComplaintBackendApplication.java
@@ -0,0 +1,13 @@
+package com.digitalcomplaint.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalComplaintBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalComplaintBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/digitalcomplaint/backend/DigitalComplaintBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalcomplaint.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalComplaintBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
